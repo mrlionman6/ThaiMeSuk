df87c639b2cbd85615e78d416b095fc8dc0ece7c
mrlionman6 <m.r.lionman6@gmail.com>
2026-08-26 21:21:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,40 @@ st.title("🇹🇭 ผู้ช่วยข้อมูลนำเข้า-ส
 st.write("ทดสอบเชื่อมต่อ Claude API")
 
 # รับ API Key จากผู้ใช้ (ชั่วคราวสำหรับทดสอบ)
-api_key = st.text_input("ใส่ Anthropic API Key", type="password")
+# api_key = st.text_input("ใส่ Anthropic API Key", type="password")
 
-if api_key:
-    client = anthropic.Anthropic(api_key=api_key)
+# api secert
+api_key = anthropic.Anthropic(api_key=st.secrets["ANTHROPIC_API_KEY"])
+
+user_input = st.text_input("พิมพ์คำถามเกี่ยวกับการนำเข้า-ส่งออก")
+
+if user_input:
+    with st.spinner("กำลังคิด")ซ
+        response = client.messages.create(
+            model = "claude-haiku-4-5-20251001",
+            max_tokens=500,
+            messages=[
+                {"role":"user","content":user_input}
+            ]
+        )
+        st.write("**คำตอบ**")
+        st.write(response.content[0].text)
+
+
+# if api_key:  # รับส่งwindows context
+#     client = anthropic.Anthropic(api_key=api_key)
     
-    user_input = st.text_input("ลองพิมพ์คำถามดู เช่น 'สวัสดี'")
+#     user_input = st.text_input("ลองพิมพ์คำถามดู เช่น 'สวัสดี'")
     
-    if user_input:
-        with st.spinner("กำลังคิด..."):
-            response = client.messages.create(
-                model="claude-haiku-4-5-20251001",
-                max_tokens=200,
-                messages=[
-                    {"role": "user", "content": user_input}
-                ]
-            )
-            st.write("**คำตอบ:**")
-            st.write(response.content[0].text)
+#     if user_input:
+#         with st.spinner("กำลังคิด..."):
+#             response = client.messages.create(
+#                 model="claude-haiku-4-5-20251001",
+#                 max_tokens=200,
+#                 messages=[
+#                     {"role": "user", "content": user_input}
+#                 ]
+#             )
+#             st.write("**คำตอบ:**")
+#             st.write(response.content[0].text)
+
